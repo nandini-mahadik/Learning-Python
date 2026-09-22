ac89789a0b4b6df97b3f7f9169c30ac04e2dc0db
Nandini Mahadik <nandinimahadik04@gmail.com>
2026-09-22 23:39:38 +0530

    Solved another problem using while loop

---
+++ lec5_2.py
@@ -0,0 +1,13 @@
+#print the elements of the following lis using a loop
+
+n = [1,4,9,16,25,36,49,64,81,100]
+i = 0
+while i < len(n):
+    print(n[i])
+    i += 1
+
+sub = ["pyhton", "c", "c++", "java"]
+i = 0
+while i < len(sub):
+    print(sub[i])
+    i += 1
