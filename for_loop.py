f60f70a96990231cb935db76d0883611ff677be3
Nandini Mahadik <nandinimahadik04@gmail.com>
2026-09-23 00:19:03 +0530

    solved problems using for loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+# Use of for loop
+
+print("Problem 1")
+names = ["om", "shruti", "pranita", "nandini", "akshaya"]
+for val in names:
+    print(val)
+
+print("Problem 2")
+tup = (1, 2, 5,8 ,2,6,3,9,7,2,)
+for num in tup:
+    print(num)
+
+print("Problem 3")
+str = "Nandini Mahadik"
+for char in str:
+    print(char)
