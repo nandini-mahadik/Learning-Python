e689315aef6325768a55b62139a7e49a264667a3
Nandini Mahadik <nandinimahadik04@gmail.com>
2026-09-23 19:31:34 +0530

    solved some questions of for loop using range function

--- for_loop.py
+++ for_loop.py
@@ -22,11 +22,39 @@
 #     print(i)
 
 #search for a number x in this tuple
-n = (1, 4, 16, 25, 36, 49, 64, 81, 100, 36)
-x = 36
-idx = 0
-for i in n:
-    if(x == i):
-        print("x found at index at", idx)
-    idx += 1
-    
+# n = (1, 4, 16, 25, 36, 49, 64, 81, 100, 36)
+# x = 36
+# idx = 0
+# for i in n:
+#     if(x == i):
+#         print("x found at index at", idx)
+#     idx += 1
+
+# # #use of range() function
+# for i in range(10):
+#     print(i)
+
+# for i in range(2,10):
+#     print(i)
+
+# for i in range(2,10,2):
+#     print(i)
+
+# for i in range(2, 100, 2):
+#     print(i)
+
+# #Questions
+# print("Question 1")
+# for i in range(1, 101):
+#     print(i)
+
+# print("Question 2")
+# for i in range(100, 0, -1):
+#     print(i)
+
+print("Question 3")
+print("print the multiplication table of any number")
+n = int(input("enter a number: "))
+i=1
+for i in range(1, 11):
+    print(i*n)
