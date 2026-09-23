37cc0d5652ca2df0fee9f5f6676d8ed1be3fa294
Nandini Mahadik <nandinimahadik04@gmail.com>
2026-09-23 19:51:52 +0530

    program to calculate sum and factorian on any number using loop

---
+++ Q3_loop.py
@@ -0,0 +1,27 @@
+#W.A.P. to find the sum of first n numbers using while
+# n = int(input("Enter a number: "))
+# sum = 0
+# i = 1
+# while i <= n:
+#     sum += i
+#     i += 1
+# print(sum)
+# for i in range(1,n+1):
+#     print(i)
+#     sum += i
+# print("total sum is", sum)
+
+#W.A.P. to find the factorial of first n numbers using while
+n = int(input("Enter a number: "))
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+print(fact)
+
+
+# for i in range(1,n+1):
+#     print(i)
+#     fact *= i
+# print("factorial is", fact)
